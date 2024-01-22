188d1d7c33eb2b0aa64a4e58e98da588f8879ef3
ALUMNO CCC - TARDE <ALUMNO CCC - TARDE@AL12034>
2024-01-22 19:18:05 +0100

    001 - initial configuration project

---
+++ src/main/java/HolaMundo.java
@@ -0,0 +1,16 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Chamberos");
+		 boolean opcion = true;
+		do {
+			System.out.println("Menu de Chambas");
+			System.out.println("Matón");
+			System.out.println("Camello");
+			
+		} while (opcion == false);
+
+	}
+
+}
