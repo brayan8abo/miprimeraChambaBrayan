94bd8d90a004455d1487385dbc217ba24c8578b2
ALUMNO CCC - TARDE <ALUMNO CCC - TARDE@AL12040>
2024-01-22 20:08:56 +0100

    003 mi cambio Nowels

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,14 +1,42 @@
+import java.util.Scanner;
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Chamberos");
-		 boolean opcion = true;
+		Scanner sc = new Scanner(System.in);
+		boolean opcion = true;
+		int opcion2;
 		do {
+
 			System.out.println("Menu de Chambas");
-			System.out.println("Matón");
-			System.out.println("Camello");
+			System.out.println("1-Matón");
+			System.out.println("2-Camello");
+			System.out.println("3-Asesino");
+			opcion2 = sc.nextInt();
+
+			if (opcion2 == 2) {
+
+				System.out.println("AHORA ERES UN CAMELLO");
+
+			}
+
+			if (opcion2 == 1) {
+
+				System.out.println("AHORA ERES UN MATÓN");
+
+			}
+
+			if (opcion2 == 3) {
+
+				System.out.println("AHORA ERES UN ASESINO");
+
+			}
 			
+			else {
+				System.out.println("ERES UN DON NADIE!!!!!!!!!!!!!!!!!");
+			}
+
 		} while (opcion == false);
 
 	}
